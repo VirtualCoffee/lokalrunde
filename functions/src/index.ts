534189c455ab3e9f6793033d9e2fb9b41eab9849
Rüdiger Weritz <excribe@gmail.com>
2020-03-21 22:18:49 +0100

    change of findPlaces function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,26 +1,28 @@
-import * as functions from 'firebase-functions';
-import * as https from 'https';
+import * as functions from "firebase-functions";
 
-export const findPlace = functions.https.onRequest(async (request, response) => {
-    return new Promise ((resolve, reject) => {
-        const key = functions.config().gmaps.key;
-        const place =  request.query.search || 'Bierbrunnen';
-        const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${place}&key=${key}&inputtype=textquery&fields=name,geometry,formatted_address,icon,photos`
-        
-        const req = https.get(url);
+export const findPlace = functions.https.onRequest(
+  async (request, response) => {
+    const key = functions.config().gmaps.key;
+    const place = request.query.search || "Bierbrunnen";
+    const path = `/maps/api/place/findplacefromtext/json?input=${place}&key=${key}&inputtype=textquery&fields=name,geometry,formatted_address,icon,photos`;
 
-        let data = '';
+    const axios = require("axios");
 
-        req.on('response', res => {
-            res.on('data', (d: any) => {
-                data += d;
-                response.send(data);
-                resolve(res);
-            });
-        });
-        
-        req.on('error', err => {
-          reject(err);
-        });
-      }); 
-});
+    const instance = axios.create({
+      baseURL: "https://maps.googleapis.com"
+    });
+
+    instance
+      .get(path)
+      .then(function(res: any) {
+        response.send(res);
+        Promise.resolve(res);
+      })
+      .catch(function(error: any) {
+        Promise.reject(error);
+      })
+      .then(function() {
+        // always executed
+      });
+  }
+);
