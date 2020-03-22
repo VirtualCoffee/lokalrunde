22aba2a63ad2d4c890698a1c17a63c1d9e3a5aba
Nico Gräf <graef.nico@gmail.com>
2020-03-22 23:55:55 +0100

    fixed donation link null error

--- app/src/app/donate/donate.component.ts
+++ app/src/app/donate/donate.component.ts
@@ -1,16 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatStepper } from '@angular/material/stepper';
-import { ActivatedRoute } from '@angular/router';
-import { Product, LocationDetail } from "../model/base"
-import { FirebaseService } from '../service/firebase.service';
+import { Component, ViewChild } from "@angular/core";
+import { MatStepper } from "@angular/material/stepper";
+import { ActivatedRoute } from "@angular/router";
+import { Product, LocationDetail } from "../model/base";
+import { FirebaseService } from "../service/firebase.service";
 
 @Component({
-  selector: 'app-donate',
-  templateUrl: './donate.component.html',
-  styleUrls: ['./donate.component.scss']
+  selector: "app-donate",
+  templateUrl: "./donate.component.html",
+  styleUrls: ["./donate.component.scss"]
 })
 export class DonateComponent {
-  @ViewChild('stepper', {static: true}) stepper: MatStepper;
+  @ViewChild("stepper", { static: true }) stepper: MatStepper;
   public placeId: string;
   public productId: string;
   public placeDetails: LocationDetail;
@@ -23,29 +23,41 @@ export class DonateComponent {
     firebaseService.getPlaceDetails(this.placeId).subscribe(results => {
       this.placeDetails = results[0];
     });
-    firebaseService.getProduct(this.placeId, this.productId).subscribe(result => {
-      this.product = result;
-    });
+    firebaseService
+      .getProduct(this.placeId, this.productId)
+      .subscribe(result => {
+        this.product = result;
+      });
   }
 
-  acceptTerms({checked}: any) {
+  acceptTerms({ checked }: any) {
     localStorage.acceptTerms = String(checked);
     this.termsAccepted = checked;
   }
 
   pay() {
-    const { donationLink, paypalAccountReceiver } = this.placeDetails;
-    const price = this.product.price.toFixed(2)
-    const trimmedDonationLink = donationLink.slice(0, donationLink.endsWith("/") ? -1 : donationLink.length);
+    const { donationLink, paypalAccountReceiver } = this.placeDetails;
+    const price = this.product.price.toFixed(2);
 
     if (paypalAccountReceiver) {
-      const returnURL = location.href.replace(/donate.*$/, "completed-donation");
-      window.open(`https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=${paypalAccountReceiver}&currency_code=EUR&amount=${price}&return=${returnURL}&item_name=${this.product.name}`, "_self");
-    } else if (trimmedDonationLink.includes("paypal.me")) {
+      const returnURL = location.href.replace(
+        /donate.*$/,
+        "completed-donation"
+      );
+      window.open(
+        `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=${paypalAccountReceiver}&currency_code=EUR&amount=${price}&return=${returnURL}&item_name=${this.product.name}`,
+        "_self"
+      );
+    } else if (donationLink.includes("paypal.me")) {
+      const trimmedDonationLink = donationLink.slice(
+        0,
+        donationLink.endsWith("/") ? -1 : donationLink.length
+      );
+
       window.open(`${trimmedDonationLink}/${price}EUR`, "_blank");
       this.stepper.next();
-    } else  {
-      console.error("Can not make donation. Missing paypal address / account")
+    } else {
+      console.error("Can not make donation. Missing paypal address / account");
     }
   }
 }
