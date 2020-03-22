fda6828815e10ca3ed7bcbb3986619d7282fee8b
Nico Gräf <graef.nico@gmail.com>
2020-03-22 22:09:07 +0100

    place function as callable function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,9 +16,19 @@ export const places = functions
     response.send(places)
   })
 
-export const place = functions.region('europe-west1').https.onRequest(async (request, response) => {
-  const googlePlaceId: string = request.query.googlePlaceId
-  const place = await getPlace(gmapsApiKey, googlePlaceId)
+export const place = functions
+  .region('europe-west1')
+  .https.onCall(async (data: { googlePlaceId: string }, context) => {
+    if (!data.googlePlaceId)
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'You have to provide a google place id (googlePlaceId).'
+      )
 
-  response.send(place)
-})
+    console.log('data', data)
+    console.log('place id', data.googlePlaceId)
+
+    const place = await getPlace(gmapsApiKey, data.googlePlaceId)
+
+    return place
+  })
